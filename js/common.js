fd484ab3dfea4ca152408ecd7be73a31a6fa2811
smthspecial <piruvat1@gmail.com>
2018-10-24 14:04:48 +0300

    start.html every screen

--- js/common.js
+++ js/common.js
@@ -9,3 +9,18 @@ document.body.addEventListener("click", (event) => {
 
     
 });
+
+const createArr =() => {
+    const randInt = (max) => Math.floor(Math.random() * max);
+
+    const arr = [];
+    for (let i = randInt(10); i--;) {
+        arr.push(randInt(100));
+        console.log(i);
+        console.log(arr);
+
+    }
+    return arr;
+}
+
+console.log(...createArr());
